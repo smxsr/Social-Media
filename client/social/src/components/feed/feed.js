2ef83f55de73cc76c338a02b5c3ffa38aa0e370f
smx <smxsrsr@gmail.com>
2022-01-05 22:19:35 +0330

    4 commit

---
+++ client/social/src/components/feed/feed.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./feed.css";
+const feed = () => {
+  return <div>feed</div>;
+};
+
+export default feed;
---
+++ client/social/src/components/rightbar/righbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./rightbar.css";
+const righbar = () => {
+  return <div>rightbar</div>;
+};
+
+export default righbar;
